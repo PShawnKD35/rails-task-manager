93439b92a8125dfeebaa4747e73ca7e87aca3b32
Shawn Peng <shawn-peng@outlook.com>
2019-07-23 14:44:40 +0800

    Added permit for post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,15 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def create
+    Task.create(task_params)
+    redirect_to Task
+  end
+
+  private
+  
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
