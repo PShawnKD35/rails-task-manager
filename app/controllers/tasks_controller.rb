ff30d8a78548a3e39142895cad62c8c3f0abd4d9
Shawn Peng <shawn-peng@outlook.com>
2019-07-23 16:08:48 +0800

    Completed optional

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 class TasksController < ApplicationController
+  before_action :task_find, only: %i[show edit update destroy]
+
   def index
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  def show; end
 
   def new
     @task = Task.new
@@ -16,25 +18,25 @@ class TasksController < ApplicationController
     redirect_to Task
   end
 
-  def edit
-    @task = Task.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to @task
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to Task
   end
 
   private
-  
+
   def task_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def task_find
+    @task = Task.find(params[:id])
   end
 end
